d2bb90a3283dd528c77bbad2a0e328b4475dc019
Edgar Luque <git@edgarluque.com>
2024-10-25 13:53:46 +0200

    prog

--- src/elf.rs
+++ src/elf.rs
@@ -2,7 +2,7 @@ use std::{fs::File, io::BufWriter};
 
 use object::{build, elf, write::StreamingBuffer};
 
-pub fn build_object() {
+pub fn build_object(program: &[u8]) {
     let mut builder = build::elf::Builder::new(object::Endianness::Little, true);
     builder.header.e_type = elf::ET_REL;
     builder.header.e_machine = elf::EM_X86_64;
@@ -24,17 +24,7 @@ pub fn build_object() {
     section.sh_flags = (elf::SHF_ALLOC | elf::SHF_EXECINSTR) as u64;
     section.sh_addralign = 16;
     // program data goes here.
-    let my_func_data: Vec<u8> = vec![
-        0xf3, 0x0f, 0x1e, 0xfa, // endbr64
-        0x55, // push   %rbp
-        0x48, 0x89, 0xe5, // mov    %rsp,%rbp
-        0xc7, 0x45, 0xfc, 0x00, 0x00, 0x00, 0x00, //  movl   $0x0,-0x4(%rbp)
-        0xb8, 0x02, 0x00, 0x00, 0x00, // mov    $0x2,%eax
-        0x5d, // pop    %rbp
-        0xc3, // ret
-    ];
-    let my_func_data_size = my_func_data.len();
-    section.data = build::elf::SectionData::Data(my_func_data.into());
+    section.data = build::elf::SectionData::Data(program.into());
     let text_id = section.id();
 
     let section = builder.sections.add();
@@ -53,7 +43,7 @@ pub fn build_object() {
     let symbol = builder.symbols.add();
     symbol.name = b"main"[..].into();
     symbol.set_st_info(elf::STB_GLOBAL, elf::STT_FUNC);
-    symbol.st_size = my_func_data_size as u64;
+    symbol.st_size = program.len() as u64;
     symbol.section = Some(text_id);
     // offset to the function within .text
     // Note: i think it needs to be 16 byte aligned
@@ -69,13 +59,14 @@ pub fn build_object() {
 mod tests {
     use std::path::Path;
 
-    use crate::linker::link_binary;
+    use crate::{assemble, linker::link_binary};
 
     use super::*;
 
     #[test]
     fn it_works() {
-        build_object();
+        let program = assemble().unwrap();
+        build_object(&program);
         link_binary(&["out.o".into()], Path::new("out")).unwrap();
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,28 @@
 pub mod elf;
 pub mod linker;
+
+pub fn assemble() -> std::result::Result<Vec<u8>, Box<dyn std::error::Error>> {
+    use iced_x86::code_asm::*;
+
+    let mut a = CodeAssembler::new(64)?;
+
+    /*
+        endbr64
+        push   rbp
+        mov    rbp,rsp
+        mov    DWORD PTR [rbp-0x4],0x0
+        mov    eax,0x2
+        pop    rbp
+        ret
+    */
+    a.endbr64()?;
+    a.push(rbp)?;
+    a.mov(rbp, rsp)?;
+    a.mov(dword_ptr(rbp - 0x4), 0)?;
+    a.mov(eax, 2u32)?;
+    a.pop(rbp)?;
+    a.ret()?;
+
+    let bytes = a.assemble(0x0)?;
+    Ok(bytes)
+}
